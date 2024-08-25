fc6eb1baf6631433b73b2ca5ba718689b661871e
Sibusiso07 <93.july.31@gmail.com>
2024-08-25 21:13:09 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+from models import Task
+
+app = Flask(__name__)
+
+tasks = []
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task_content = request.form.get('content')
+    if task_content:
+        tasks.append(Task(task_content))
+    return redirect(url_for('index'))
+
+
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    tasks.pop(task_id)
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,3 @@
+class Task:
+    def __init__(self, content):
+        self.content = content
